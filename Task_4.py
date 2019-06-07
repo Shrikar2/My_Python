e3949331edf7949b5aeaca78655e6ad1e3f63e44
Shrikar2 <51485991+Shrikar2@users.noreply.github.com>
2019-06-07 21:54:20 +0530

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -1,12 +1,3 @@
-list1=['January','March','May','July','August','October','December']
-list2=['April','June','August','September','November']
-list3=['February']
-for i in range(0,7):
-    if string==list1[i]:
-        print("31")
-for i in range(0,5):
-    if string==list2[i]:
-        print("30")
-if string==list3[0]:
-    print("28,if not  a leap year")
-    print("29,if a leap year")
+string=input("Enter the String:" )
+print(string.upper())
+print(string.lower())
