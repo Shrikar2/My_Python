64c56651f88f3eabf4e83b1660a43b89a17d0037
Shrikar2 <51485991+Shrikar2@users.noreply.github.com>
2019-06-07 21:58:04 +0530

    Create Task_7.py

---
+++ Task_7.py
@@ -0,0 +1,8 @@
+print("Enter the values:" )
+value_1=input("value 1: ")
+value_2=input("value 2: ")
+value_3=input("value 3: ")
+list1=[value_1,value_2,value_3]
+list1.sort()
+median=list1[1]
+print(median)
